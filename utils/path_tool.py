4d9d9e6d43c4fcd3ec60eeb13d4445f1e46d4a93
hzt <2502712438@qq.com>
2026-04-05 19:29:29 +0800

    feat: log配置

---
+++ utils/path_tool.py
@@ -0,0 +1,32 @@
+"""
+为整个工程提供提供的绝对路径
+"""
+
+import os
+
+
+
+def get_project_root() -> str:
+    """
+    获取工程的根目录
+
+    :return:
+    """
+    # 当前文件绝对路径
+    current_file = os.path.abspath(__file__)
+    # 获取工程的跟目录
+    current_dir = os.path.dirname(current_file)
+    project_root = os.path.dirname(current_dir)
+    return project_root
+
+def get_abs_path(relative_path: str) -> str:
+    """
+    传递相对路径，得到绝对路径
+    :param relative_path:
+    :return:
+    """
+    project_root = get_project_root()
+    return os.path.join(project_root, relative_path)
+
+if __name__ == '__main__':
+    print(get_abs_path("config/config.txt"))
