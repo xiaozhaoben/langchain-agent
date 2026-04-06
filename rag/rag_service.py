b65f495bb9289751fa07226e6c5e205b1c757702
hzt <2502712438@qq.com>
2026-04-06 16:56:54 +0800

    feat: tools工具

--- rag/rag_service.py
+++ rag/rag_service.py
@@ -1,7 +1,6 @@
 """
 总结服务类: 用户提问，搜索参考资料，将提问和参考资料交给模型，让模型总结恢复
 """
-from itertools import count
 
 from langchain_core.documents import Document
 from langchain_core.output_parsers import StrOutputParser
